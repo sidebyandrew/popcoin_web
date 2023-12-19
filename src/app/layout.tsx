106575633a38869cf585da1b2ee08bd1d9f69f03
Andrew <andrew.sideby@gmail.com>
2023-12-19 13:00:14 +0800

    use sdk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Providers } from '@/app/providers';
 import { fontSans } from '@/config/fonts';
 import { siteConfig } from '@/config/site';
-import { ContextProps } from '@/contexts/TMA';
+import { ContextProps, TMAProvider } from '@/contexts/TMA';
 import { Analytics } from '@vercel/analytics/react';
 import clsx from 'clsx';
 import type { Metadata } from 'next';
@@ -39,16 +39,16 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {/*<TMAProvider headers={headersForContext}>*/}
-        <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
-          <div className="relative flex h-screen flex-col">
-            <main className="container mx-auto max-w-7xl flex-grow ">
-              {children}
-            </main>
-          </div>
-        </Providers>
-        <Analytics />
-        {/*</TMAProvider>*/}
+        <TMAProvider headers={headersForContext}>
+          <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
+            <div className="relative flex h-screen flex-col">
+              <main className="container mx-auto max-w-7xl flex-grow ">
+                {children}
+              </main>
+            </div>
+          </Providers>
+          <Analytics />
+        </TMAProvider>
       </body>
     </html>
   );
