6297e9c50afb20ccf3a76a1043f292c08aa04781
Andrew <andrew.sideby@gmail.com>
2024-05-14 20:14:27 +0800

    showPosition22

--- src/components/tab/Tab4Content.tsx
+++ src/components/tab/Tab4Content.tsx
@@ -7,37 +7,46 @@ const Tab4Content: React.FC = () => {
   const [scrollPosition, setScrollPosition] = React.useState('');
   const [audio, setAudio] = React.useState('');
   const [blue, setBlue] = React.useState('');
+  const [loca, setLoca] = React.useState('');
 
   async function eventGo() {
     if (window) {
       setWorkWindows(window.name);
       // var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
       // setAudio(JSON.stringify(audioCtx));
-
-      const options = {
-        acceptAllDevices: true
-      };
-      // @ts-ignore
-      const device = await navigator.bluetooth.requestDevice(options);
-      console.log('Device:', device);
-      setBlue(device.id);
+      // const options = {
+      //   acceptAllDevices: true
+      // };
+      // // @ts-ignore
+      // const device = await navigator.bluetooth.requestDevice(options);
+      // console.log('Device:', device);
+      // setBlue(device.id);
       setAudio("No audio");
+    }
 
+    if  (navigator){
+      navigator.geolocation?.getCurrentPosition(showPosition)
     }
   }
 
-
+  function showPosition(position:any) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const accuracy = position.coords.accuracy;
+    setLoca(""+latitude+"-"+longitude+"-"+accuracy);
+  }
   useEffect(() => {
     eventGo();
   }, []);
 
   return (
-    <div>
+    <div className={"p-6"}>
       <div>Hello Air</div>
       <div>Window {workWindow}</div>
       <div> scrollPosition = {scrollPosition}</div>
       <div> audio = {audio}</div>
       <div> bluetooth = {blue}</div>
+      <div> location = {loca}</div>
     </div>
   );
 };
